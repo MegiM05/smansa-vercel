7021a7aac5328efffb8cd14106c7701849426a1f
MegiM05 <melasarimegi@gmail.com>
2025-10-28 20:34:49 +0900

    Delete smansa-vercel(1) directory

--- smansa-vercel(1)/api/logout.js
+++
@@ -1,5 +0,0 @@
-const { setCookie, sendJSON } = require("./_utils");
-module.exports = (req, res) => {
-  setCookie(res, "token", "", { httpOnly: true, maxAge: 0, sameSite: "Lax" });
-  sendJSON(res, 200, { ok: true });
-};
